ad517cce1bec7c9f8b2fc8d2ab6c0416853ea7a5
WarnerCo <92418771+warnerco@users.noreply.github.com>
2022-02-28 22:08:13 -0600

    Create D2B.py

---
+++ D2B.py
@@ -0,0 +1,14 @@
+#this script turns digits into binary
+import math
+
+def decimalToBinary(num):
+    if num >= 1:
+        decimalToBinary(num // 2)
+    print(num % 2, end='')
+
+
+# decimal number
+number = int(input("Enter any number (no decimal): "))
+
+decimalToBinary(number)
+
