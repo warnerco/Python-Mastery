2f4161064822fbbfded15f2ab14b4debe712ea74
WarnerCo <92418771+warnerco@users.noreply.github.com>
2022-02-28 22:08:05 -0600

    Create B2D.py

---
+++ B2D.py
@@ -0,0 +1,14 @@
+#this script turns binary into digits 
+def bd(n):
+    return int(n,2)
+
+if __name__=='__main__':
+     #print(bd('01101000'))
+     #print(bd('01100101'))
+     #print(bd('01100011'))
+     bdUser = str(input("Enter Binary Digits (8 only):"))
+print(bd(bdUser))
+
+  
+
+
