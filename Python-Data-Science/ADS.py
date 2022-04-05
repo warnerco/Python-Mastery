9ddb49f5a400ff148a33b421f86baad48383fd12
WarnerCo <92418771+warnerco@users.noreply.github.com>
2022-04-05 15:41:51 -0500

    Update April 5

---
+++ Python-Data-Science/ADS.py
@@ -0,0 +1,10 @@
+# NNW
+# APPLIED DATA SCIENCE (ADS) 
+# Coursera Textbook: Python for Data Analysis DATA WRANGLING WITH PANDAS, NUMPY, AND IPYTHON
+# Wes McKinney 2nd Edition 
+
+# Applied Data Science Part 1 Jupyter Notebook Notes (via Coursera Textbook):
+# file:///Users/master/Desktop/Applied%20Data%20Science.html
+
+# Applied Data Science Part 2 Jupyter Notebook:
+# file:///Users/master/Desktop/Applied_Data_Science_Part_2.html
